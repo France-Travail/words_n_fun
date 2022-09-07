c4147c8bb9dfe3c37a5ef704c6b88895b39e5586
A_GAREL <alexandre.garel.56@gmail.com>
2022-09-07 15:20:42 +0200

    words_n_fun (fix): fix setup

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        'pandas>=1.3,<1.5',
         'pandas>=1.3,<1.4; python_version < "3.8"',
         'pandas>=1.3,<1.5; python_version >= "3.8"',
         'numpy>=1.19,<1.22; python_version < "3.8"',
